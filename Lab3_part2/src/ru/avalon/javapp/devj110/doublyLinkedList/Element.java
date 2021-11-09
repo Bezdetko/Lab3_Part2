5902af8a3567a9e22423b3ba7f5cc19047e257a9
Bezdetko <bezdetk0@mail.ru>
2021-11-09 23:57:41 +0300

    Версия 1

---
+++ Lab3_part2/src/ru/avalon/javapp/devj110/doublyLinkedList/Element.java
@@ -0,0 +1,16 @@
+package ru.avalon.javapp.devj110.doublyLinkedList;
+
+public class Element {
+    protected int data;
+    protected Element parent;
+    protected Element child;
+
+//    public Element(int data) {
+//        this.data = data;
+//    }
+    
+    
+
+    
+    
+}
